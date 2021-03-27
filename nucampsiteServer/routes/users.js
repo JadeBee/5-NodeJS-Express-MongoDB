25f86038f638f152bd29eb4b659c97f5a63fc8b8
Jade <jadebolds97@gmail.com>
2021-03-27 17:36:09 -0400

    Week 3 BREFOR Workshop

--- nucampsiteServer/routes/users.js
+++ nucampsiteServer/routes/users.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const User = require("../models/user");
+
+const router = express.Router();
+const passport = require("passport");
+const authenticate = require("../authenticate");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  const token = authenticate.getToken({ _id: req.user._id });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json({
+    success: true,
+    token: token,
+    status: "You are successfully logged in!",
+  });
+});
+
+router.get("/logout", (req, res, next) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie("session-id");
+    res.redirect("/");
+  } else {
+    const err = new Error("You are not logged in!");
+    err.status = 401;
+    return next(err);
+  }
 });
 
 module.exports = router;
